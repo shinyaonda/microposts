773fd3c53cac7cb939e0d19e724c6c6d9d4690bd
shinyaonda <shinya.o.2010@gmail.com>
2015-10-25 01:54:46 +0000

    add user model

--- db/migrate/20151024194519_create_users.rb
+++ db/migrate/20151024194519_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :paswoed_digest
+      t.string :password_digest
 
       t.timestamps null: false
       
